c6298c4186d6a61516a14a949c80d671f6db5eb3
delobelchloe <chloe.delobel@edu.itescia.fr>
2021-02-04 12:27:00 +0100

    Update Inscription.php

--- Inscription.php
+++ Inscription.php
@@ -61,3 +61,62 @@ if($valid){
     exit;
 }
 ?>
+
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="StyleInscription.css"/>
+    <title>Clothes Shop</title>
+   <header><div id= "ClothesShop-section-header" class= "ClothesShop-section">
+    <nav class="slide-nav_wrapper">
+    <ul id="SlideNav" class="slide-nav">
+    <li class="slide-nav_item border-bottom">
+    <a> Acceuil </a>
+    </li>
+    <li class="slide-nav__item meduim-up--hide">
+    <button type="button" value="Achat"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
+</svg>
+    Mon panier
+    </a>
+    <button type="button" value="Connexion" >
+    Se connecter 
+    </a>
+    </div>
+    </header>
+    <body>
+
+    <form action="/ma-page-de-inscription" method="post">
+    <div>
+        <label for="name">Pseudo :</label>
+        <input type="text" id="name" name="user_pseudo">
+    </div>
+    <div>
+        <label for="VilleCli">Ville :</label>
+        <textarea id="text" name="user_VilleClient"></textarea>
+    </div>   
+    <div>
+        <label for="DateNaiss">Date de naissance :</label>
+        <textarea id="text" name="user_DateNaissance"></textarea>
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="Mdp">Mot de passe :</label>
+        <input type="password" id="pwd" name="user_password">
+    </div>
+    <div>
+        <label for="Mdp">Confirmation du mot de passe :</label>
+        <input type="password" id="pwd" name="user_ConfirmPassword">
+    </div>
+
+
+    </body>
+
+    </html>
