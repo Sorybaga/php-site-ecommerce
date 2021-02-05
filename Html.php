59bfe539f6de147220a38b118d4c7fbc038fc96c
Sory <sorybagayoko5@gmail.com>
2021-02-05 09:21:01 +0100

    Auto stash before merge of "main" and "origin/main"

--- Html.php
+++ Html.php
@@ -1,9 +1,11 @@
 <?php
+
 session_start();
 if (!isset($_SESSION["pseudo"])) {
     header("Location: Connexion.php");
     exit();
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +26,7 @@ if (!isset($_SESSION["pseudo"])) {
     <button type="button" value="Achat"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
   <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
 </svg>
+    <button type="button" value="Mon panier">
     Mon panier
     </a>
     <button type="button" value="Inscription">
@@ -44,11 +47,6 @@ if (!isset($_SESSION["pseudo"])) {
 <img src="Downloads/Sweat1" alt ="Sweat Homme 1"/><img src="Downloads/JeanH2" alt="Jean Homme 2"/>
 
 </div>
-
-
-
-
-
 
 
 </body>
